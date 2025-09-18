5d888e5c79bebd2e0d7009009323478923812cfb
Suganya <Suganya@Suganya>
2025-09-18 20:30:28 +0530

    Test NG Project- Installation & Test annotation with priority.

---
+++ SeleniumLearning/src/testNG/SampleTestcase.java
@@ -0,0 +1,23 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTestcase {
+	@Test
+	public void firstTestCase() {
+		System.out.println("This is the first test case");
+	}
+	@Test
+	public void SecondTestCase() {
+		System.out.println("This is the Second test case");
+	}
+	@Test
+	public void ThirdTestCase() {
+		System.out.println("This is the Third test case");
+	}
+	@Test
+	public void FouthTestCase() {
+		System.out.println("This is the Fourth test case");
+	}
+
+}
