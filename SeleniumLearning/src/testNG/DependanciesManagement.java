5396b7404a3446b7c3064a4a555dfe03d60b12b7
Suganya <Suganya@Suganya>
2025-09-27 16:46:51 +0530

    TestNG- Dependencies

---
+++ SeleniumLearning/src/testNG/DependanciesManagement.java
@@ -0,0 +1,25 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class DependanciesManagement {
+	//Problem 1: you have to admit a student to engineering college
+	// Problem 2: you have to admit a student to Higher secondary
+	
+	//enabled = false is used for skipping a testcase
+	
+	@Test(enabled=false)// if false below methods won't execute 
+	public void highschool() {
+		System.out.println("High School");
+	}
+	@Test(dependsOnMethods ="highschool")
+	public void HigherSecondary() {
+		System.out.println("higher Secondary School");
+	}
+	@Test(dependsOnMethods = "HigherSecondary")
+	public void Engineering() {
+		System.out.println("College");
+	}
+	
+
+}
